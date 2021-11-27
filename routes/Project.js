8d286a347223763ee5cff01b051493a8ee7339df
LRojon <loic.rojon@gmail.com>
2021-11-27 15:56:08 +0100

    Add project/update request

--- routes/Project.js
+++ routes/Project.js
@@ -63,4 +63,32 @@ router.post('/create', async (req, res) => {
     }
 })
 
+/**
+ * POST
+ * URL: /project/update
+ * 
+ * params:
+ *      {
+ *          _id: String(32),
+ *          name: String,
+ *          presentation: String,
+ *          overview: String
+ *      }
+ * 
+ * Modifie un project déjà existant
+ * 
+ */
+router.post('/update', async (req, res) => {
+    if(req.body._id.length != 32) { (new Error(400, 'INC', { id: req.body._id })).send(res) }
+    let p = await Project.fromRequest(req.body)
+    const projects = await sql.query('SELECT _id FROM Project WHERE _id = "' + req.body._id + '"')
+
+    if(projects.length <= 0) { (new Error(400, 'NEX', { element: 'project' })).send(res) }
+    let query = 'UPDATE Project SET name = "' + p.name + '", presentation = "' + p.presentation + '", overview = "' + p.overview + '" WHERE _id = "' + p._id + '";'
+    const r = await sql.query(query)
+    
+    if(r.affectedRows === 1) { res.status(200).send('Updated successfully') }
+    else { (new Error(500, 'ENL')).send(res) }
+})
+
 module.exports = router
